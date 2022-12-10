1a2b314ec44f6e2b3ec30ba5dee022e88816b9f4
EternalII <Lior_acolyte@walla.com>
2022-12-10 19:58:01 +0200

    Changed coin name
    from Mica to RL

--- blockchain_end/main.js
+++ blockchain_end/main.js
@@ -5,16 +5,16 @@ const ec = new EC('secp256k1')
 const myKey = ec.keyFromPrivate('4a8c9b48ffb71cae5f8663074b4a7d32a1ed50b4f16b9e12520f96ce4f4b7d22')
 const myWalletAddress=myKey.getPublic('hex')
 
-let micaCoin=new Blockchain()
+let RLCoin=new Blockchain()
 const tx1=new Transaction(myWalletAddress,'address1',10)
 tx1.signTransaction(myKey)
-micaCoin.addTransaction(tx1)
+RLCoin.addTransaction(tx1)
 
-micaCoin.minePendingTransactions(myWalletAddress)
+RLCoin.minePendingTransactions(myWalletAddress)
  
  
  
 
-console.log('\ Balance of Bob ', micaCoin.getBalanceOfAddress(myWalletAddress))
+console.log('\ Balance of Bob ', RLCoin.getBalanceOfAddress(myWalletAddress))
 
-console.log(JSON.stringify(micaCoin, null, 4))
+console.log(JSON.stringify(RLCoin, null, 4))
